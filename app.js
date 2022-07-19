b781e2ad71660cbc6fd4aafbfd82305ee6c2aa28
yasirrifai <76211378+yasirrifai@users.noreply.github.com>
2022-07-20 03:59:13 +0530

    passport

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.use(session({
-  secret: 'longtextstring.',
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false,
 }));
